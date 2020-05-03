6bba08c4b18e241314c79f4d5f7b213b85c771bd
Pauwlus <42134561+Pauwlus@users.noreply.github.com>
2020-05-03 14:46:21 +0200

    10d
    10d

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ export default () => (
     <div>===========</div>
     <div> </div>
     Hallo Wereld.
-    <div>Versie 10c</div>
+    <div>Versie 10d</div>
     <Link href="/about">
       <a>Over</a>
     </Link>
