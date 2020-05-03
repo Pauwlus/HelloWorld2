7a7dffb068906eff6279c7f6bb1e7803451541ca
Pauwlus <42134561+Pauwlus@users.noreply.github.com>
2020-05-03 14:00:09 +0200

    Pagina 5
    Pagina 5 toegevoegd

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import Link from "next/link";
 export default () => (
   <div>
-    Hallo Wereld.{" "}
+    Hallo Wereld.
+    <div />
     <Link href="/about">
       <a>Over</a>
+    </Link>
+    <div />
+    Hallo Dag.{" "}
+    <Link href="/day">
+      <a>Dag</a>
     </Link>
   </div>
 );
